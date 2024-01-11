ae875529631177490c209ce15bb4a7fcfb4b8613
natanon-jaru <149050860+natanon-jaru@users.noreply.github.com>
2024-01-11 10:54:56 +0700

    +Code Script
    +DisplayInventory (Array to List)
    +press A to add helmet
    +press R to remove sword

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Test : MonoBehaviour
 {
+    //Inventory array
+    private string[] inventoryArray = new[] { "Sword", "Shield", "Potion" };
+    
+    //move elements in the array to list
+    private List<string> inventoryList;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventoryList = new List<string>(inventoryArray);
+        DisplayInventory();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            inventoryList.Add("Helmet");
+            DisplayInventory();
+        }
         
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            inventoryList.Remove("Sword");
+            DisplayInventory();
+        }
+    }
+
+    void DisplayInventory()
+    {
+        int i = 1;
+        Debug.Log("Inventory: ");
+        foreach (var item in inventoryList)
+        {
+            Debug.Log($"{i++}. {item}");
+        }
     }
 }
